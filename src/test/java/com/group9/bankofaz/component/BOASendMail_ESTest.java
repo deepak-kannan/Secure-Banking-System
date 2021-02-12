fb4b941269c95a3df6732fb55acd0d3ea0144b2d
csidevops <56381436+csidevops@users.noreply.github.com>
2021-02-12 09:38:39 +0530

    Update BOASendMail_ESTest.java

--- src/test/java/com/group9/bankofaz/component/BOASendMail_ESTest.java
+++ src/test/java/com/group9/bankofaz/component/BOASendMail_ESTest.java
@@ -2,14 +2,10 @@ package com.group9.bankofaz.component;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import com.group9.bankofaz.component.BOASendMail;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.junit.runner.RunWith;
 
-@RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = true, useJEE = true) 
-public class BOASendMail_ESTest extends BOASendMail_ESTest_scaffolding {
+public class BOASendMail_ESTest {
 
   @Test(timeout = 4000)
   public void test0()  throws Throwable  {
